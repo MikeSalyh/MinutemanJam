69fed0c02f08e2b7a51e5c46596e6e04322d3865
Mike Salyh <mike@salyh.com>
2022-09-30 18:33:23 -0700

    Dodging mechanic.

--- Minuteman/Assets/Scripts/PlayerCharacter.cs
+++ Minuteman/Assets/Scripts/PlayerCharacter.cs
@@ -6,53 +6,88 @@ public class PlayerCharacter : MonoBehaviour
 {
     public float maxSpeed = 150f;
     public float acceleration = 15f;
-    public float speedFalloff = 2f;
+    public float speedFriction = 1.1f;
+    public float dodgeCooldown = 1f;
+    public float dodgeAcceleration = 100f;
+
+    private float lastDodgeTime = 0f;
+    private Vector2 _inputVector = new Vector2();
     private Vector2 _moveVector = new Vector2();
+    private Vector2 _dodgeVector = new Vector2();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        HandleMovement();
+        HandleMovementInput();
     }
 
-    private void HandleMovement()
+    private void FixedUpdate()
     {
-        Vector2 directionalVector = new Vector2();
+        HandleMovementPhysics();
+    }
+
+    private void HandleMovementInput()
+    {
+        //Move, accelerate + decelerate the character
+        _inputVector = new Vector2();
+        if (Time.time < lastDodgeTime + dodgeCooldown)
+            return;
+
         if (Input.GetKey(KeyCode.W))
         {
-            directionalVector += Vector2.up * acceleration;
-        } else if (Input.GetKey(KeyCode.S))
-        {
-            directionalVector += Vector2.down * acceleration;
+            _inputVector += Vector2.up * acceleration;
         }
-        
+        else if (Input.GetKey(KeyCode.S))
+        {
+            _inputVector += Vector2.down * acceleration;
+        }
+
 
         if (Input.GetKey(KeyCode.A))
         {
-            directionalVector += Vector2.left * acceleration;
-        } else if (Input.GetKey(KeyCode.D))
-        {
-            directionalVector += Vector2.right * acceleration;
+            _inputVector += Vector2.left * acceleration;
         }
-        directionalVector.Normalize();
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _inputVector += Vector2.right * acceleration;
+        }
+        _inputVector.Normalize();
 
+    }
 
-        _moveVector += directionalVector * acceleration;
-        if (directionalVector.x == 0)
-            _moveVector.x /= speedFalloff;
+    private void HandleMovementPhysics()
+    {
+        _moveVector += _inputVector * acceleration;
+        if (_inputVector.x == 0)
+            _moveVector.x /= speedFriction;
 
-        if (directionalVector.y == 0)
-            _moveVector.y /= speedFalloff;
+        if (_inputVector.y == 0)
+            _moveVector.y /= speedFriction;
 
         _moveVector.x = Mathf.Clamp(_moveVector.x, -maxSpeed, maxSpeed);
         _moveVector.y = Mathf.Clamp(_moveVector.y, -maxSpeed, maxSpeed);
 
-        transform.Translate(_moveVector * Time.deltaTime);
+        transform.Translate(_moveVector);
+
+
+        //Dodging
+        if (Input.GetMouseButton(1) && _moveVector.magnitude > 1f)
+        {
+            if (Time.time > lastDodgeTime + dodgeCooldown)
+            {
+                lastDodgeTime = Time.time;
+                _dodgeVector = _moveVector.normalized * dodgeAcceleration;
+                Debug.Log("Dodging!");
+            }
+        }
+
+        transform.Translate(_dodgeVector);
+        _dodgeVector /= speedFriction;
     }
 }
